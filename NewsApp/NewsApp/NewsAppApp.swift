b82a9c77baa061aea4df1f7b643edd2b8fa924d9
Minny Joseph <64777938+minnyjoseph03@users.noreply.github.com>
2025-01-26 22:50:41 +0530

    Add files via upload

---
+++ NewsApp/NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Apple on 26/01/25.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
